fd42d85d5a447b1bfeca6e2723fefb745bab82c5
Medhansh Kabadwal <medhansh5@proton.me>
2026-04-16 08:21:22 +0530

    Update map.js
    Update fetch link for online data.

--- static/js/map.js
+++ static/js/map.js
@@ -2,7 +2,7 @@
 const map = L.map('map').setView([28.6692, 77.3538], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+    attribution: 'Â© OpenStreetMap contributors'
 }).addTo(map);
 
 // Color mapping for road quality
@@ -13,7 +13,7 @@ const colors = {
 };
 
 // Fetch data from our API
-fetch('/roads')
+fetch('https://shadowmap-api.onrender.com/roads')
     .then(response => response.json())
     .then(data => {
         data.forEach(point => {
